afe401c47d10ee30475bb3687dda97a9aa5af05c
sunilvaishnav6767 <Sunilvaishnav55004@gmail.com>
2026-07-03 01:43:27 +0530

    Implement lightbox functionality for image gallery

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
     console.log("Achiever's Arena Library Website Loaded Successfully");
 });
+const images = document.querySelectorAll(".gallery-grid img");
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+const closeBtn = document.querySelector(".close");
+
+images.forEach(img => {
+  img.addEventListener("click", () => {
+    lightbox.style.display = "flex";
+    lightboxImg.src = img.src;
+  });
+});
+
+closeBtn.addEventListener("click", () => {
+  lightbox.style.display = "none";
+});
+
+lightbox.addEventListener("click", (e) => {
+  if (e.target === lightbox) {
+    lightbox.style.display = "none";
+  }
+});
